eb9e8fc5f460f4ebcad3e3dedfbe760e100a2e86
Laura Alves <lauraalves.contato@gmail.com>
2025-09-09 17:56:08 -0300

    Init nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- VIM to LUA command: vim.cmd([COMMAND GOES HERE])
+
+vim.g.mapleader = ' '                               -- use SPACEBAR as leader
+vim.opt.number = true                               -- show line numbers
+
+vim.opt.tabstop = 4                                 -- 4 spaces tab
+vim.opt.shiftwidth = 4                              -- 4 spaces indentation
+vim.opt.expandtab = true                            -- use spaces when tabbing
