94e119cc398fd7db861a8b718bac19e6bdeaf2f3
Laura Alves <lauraalves.contato@gmail.com>
2025-09-10 18:04:13 -0300

    Add vim cheat sheet

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,27 @@
 -- VIM to LUA command: vim.cmd([COMMAND GOES HERE])
 
-vim.g.mapleader = ' '                               -- use SPACEBAR as leader
-vim.opt.number = true                               -- show line numbers
+-- useful aliases
+local map = vim.keymap.set
+local set = vim.opt
 
-vim.opt.tabstop = 4                                 -- 4 spaces tab
-vim.opt.shiftwidth = 4                              -- 4 spaces indentation
-vim.opt.expandtab = true                            -- use spaces when tabbing
+vim.g.mapleader = ' '       -- use SPACEBAR as leader
+set.number = true           -- show line numbers
+
+set.tabstop = 4             -- 4 spaces tab
+set.shiftwidth = 4          -- 4 spaces indentation
+set.expandtab = true        -- use spaces when tabbing
+
+-- keymap
+map('i', '{<cr>', '{<cr>}<esc>kA<cr><Tab>', {
+    noremap = true,
+    silent = true,
+    desc = 'Auto-indent and place closing curly brackets upon inserting { and then <cr>'
+}
+)
+
+map('n', '<leader>o', 'o<esc>', {
+    noremap = true,
+    silent = true,
+    desc = 'Place a new line under cursor while remaining in normal mode'
+}
+)
--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,12 @@ local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE" -- remove titlebar
 config.use_fancy_tab_bar = false -- use compact tab bar
+config.window_padding = { -- remove padding
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
 
 -- theme settings
 config.color_scheme = 'Catppuccin Latte'
