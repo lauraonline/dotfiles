f4228c8cb18931e9a6d512fd8e8fa428123f93a2
Laura Alves <lauraalves.contato@gmail.com>
2025-09-12 01:38:46 -0300

    Add auto-apply shell script, remove wezterm config

---
+++ apply_all_dotfiles.sh
@@ -0,0 +1,8 @@
+# sway config
+ln -s ./.config/sway/config ~/.config/sway/config
+# nvim config
+ln -s ./.config/nvim/init.lua ~/.config/nvim/init.lua
+# zsh config
+ln -s ./.zshrc ~/.zshrc
+# pi (paste image) takes an image from your clipboard and saves it in ~/Screenshots/
+ln -s ./.local/bin/pi ~/.local/bin/pi
