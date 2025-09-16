4f57a554ba79c0430677f636e9a2f8730c6e6876
Laura Alves <lauraalves.contato@gmail.com>
2025-09-16 08:38:45 -0300

    Add rmpc config and add it to the dotfiles

--- apply_all_dotfiles.sh
+++ apply_all_dotfiles.sh
@@ -10,6 +10,7 @@ dotfiles=(
     ".config/nvim/lua/plugins/blink.lua"
     ".config/nvim/lua/plugins/nvim_highlight_colors.lua"
     ".config/nvim/lua/plugins/treesitter.lua"
+    ".config/rmpc/config.ron"
 
     ".config/fontconfig/fonts.conf"
     ".local/share/fonts/MapleMono"
@@ -24,8 +25,6 @@ dotfiles=(
     ".config/waybar/style.css"
 
     ".zshrc"
-
-    ".local/bin/pi"
 
     ".local/share/wayland-sessions/sway.desktop"
 
