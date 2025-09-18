9068a074713854fd375a271e74abc1d3cd63ad85
Laura Alves <lauraalves.contato@gmail.com>
2025-09-17 23:58:46 -0300

    meep

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,26 +1,24 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        snippets = {
-          opts = {
-            extended_filetypes = {
-              html = { 'html' }
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+        keymap = { preset = 'default' },
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            nerd_font_variant = 'mono',
+        },
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 250,
+                treesitter_highlighting = true,
             }
-          }
-        }
-      }
+        },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
     },
-  },
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
 }
